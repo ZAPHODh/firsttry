454dd2977b0951b404cbbd84ecccb7d66541bbe5
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-04-11 11:09:06 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,48 @@
+let board = [``, ``, ``, ``, ``, ``, ``, ``, ``]
+let playertime = 0;
+let symbols = [`o`, `x`]
+let gameOver = false;
+let ganhador1 = 0;
+let ganhador2 = 0;
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+function handleMove(position) {
+    if (gameOver) {
+        return;
+    }
+    if (board[position] == ``) {
+        board[position] = symbols[playertime];
+
+        gameOver = Iswin();
+
+        if (!gameOver) {
+
+            playertime = (playertime == 0) ? 1 : 0;
+        }
+    }
+    else {
+        alert("Esta caixa ja está preenchida. Por favor, selecione outra.")
+    }
+    return gameOver;
+}
+function Iswin() {
+    for (let i = 0; i < winStates.length; i++) {
+        let sequence = winStates[i];
+        let pos1 = sequence[0];
+        let pos2 = sequence[1];
+        let pos3 = sequence[2];
+        if (board[pos1] == board[pos2] &&
+            board[pos1] == board[pos3] &&
+            board[pos1] != ``) {
+            return true;
+        }
+    }
+}
